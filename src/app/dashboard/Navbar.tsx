b3c2f90448f3541bd8b626976d794d3a98a3814a
Daniel Muniz <danielnubiz902@gmail.com>
2023-08-24 09:43:00 -0400

    Add translated names to navbar header

--- src/app/dashboard/Navbar.tsx
+++ src/app/dashboard/Navbar.tsx
@@ -2,21 +2,55 @@
 
 import React from "react";
 import Image from "next/image";
-import { usePathname } from 'next/navigation'
-import userImage from "../../assets/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png";
-import logo from "../../assets/inicio.svg";
+import { usePathname } from "next/navigation";
 import { LogoutOutlined } from "@ant-design/icons";
 import { signOut, useSession } from "next-auth/react";
+
+import logo from "../../assets/inicio.svg";
+import userImage from "../../assets/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png";
 
 // TODO: Añadir logica para que el encabezado cambie de acuredor a la seccion
 
 export const Navbar = () => {
-  const pathname = usePathname()
-  const currentPath = pathname.split('/')
-  const {data:sessionData} = useSession()
-  const username = sessionData?.user?.user
-  localStorage.setItem('accessToken', sessionData?.user?.accessToken)
+  const pathname = usePathname();
+  let mainPath = pathname.split("/");
+  let secondaryPath = mainPath[2];
+  const { data: sessionData } = useSession();
+  const username = sessionData?.user?.user;
+  localStorage.setItem("accessToken", sessionData?.user?.accessToken);
+
+  if(mainPath.includes('dashboard')) mainPath[1] = 'Menú Principal' 
+
+  switch (secondaryPath) {
+    case "users":
+      secondaryPath = "Usuarios"
+      break;
+    case "nomenclators":
+      secondaryPath = "Nomencladores"
+      break;
+    case "humanResources":
+      secondaryPath = "Recursos Humanos"
+      break;
+    case "office":
+      secondaryPath = "Gastos de Oficina"
+      break;
+    case "warehouse":
+      secondaryPath = "Almacenes"
+      break;
+    case "ticketsWarehouse":
+      secondaryPath = "Almacén de Vales"
+      break;
+    case "project":
+      secondaryPath = "Proyectos"
+      break;
+    case "projectExpenses":
+      secondaryPath = "Gastos de Proyectos"
+      break;
   
+    default:
+      break;
+  }
+
   return (
     <>
       <div className="flex justify-between items-center h-[5rem] bg-primary-500 w-full absolute">
@@ -26,13 +60,24 @@ export const Navbar = () => {
             <li className="flex items-center gap-[0.5rem] font-bold text-white text-base">
               <Image src={userImage} width={50} height={50} priority={false} alt="user image" className="rounded-full" />
               <span>{`${username}`}</span>
-              <LogoutOutlined onClick={()=>{signOut()}} className="mb-0.5 text-md"/>
+              <LogoutOutlined
+                onClick={() => {
+                  signOut();
+                }}
+                className="mb-0.5 text-md"
+              />
             </li>
           </ul>
         </div>
       </div>
       <div className=" items-center p-8 flex relative top-[3rem] ml-[19.5rem] h-[4rem] rounded-md w-[70%] bg-white shadow-md">
-        <span className="font-bold text-2xl font-segoe ">{`${currentPath[1].toUpperCase()}`}</span>
+        {
+          secondaryPath 
+          ? <span className="font-bold text-2xl font-segoe ">{`${secondaryPath}`}</span> 
+          : <span className="font-bold text-2xl font-segoe ">{`${mainPath[1]}`}</span>
+        }
+        
+        
       </div>
     </>
   );
