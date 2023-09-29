07532e19a097ba52a5a1bc527f154f47598623d8
Daniel Muniz <danielnubiz902@gmail.com>
2023-09-29 09:14:47 -0400

    Add custom footer to nomenclator modals

--- src/app/dashboard/nomenclators/EditNomenclatorForm.tsx
+++ src/app/dashboard/nomenclators/EditNomenclatorForm.tsx
@@ -35,31 +35,52 @@ const category: SelectProps["options"] = [
 ];
 
 export const EditNomenclatorForm: React.FC<CollectionCreateFormProps> = ({ open, onCreate, onCancel, defaultValues }) => {
-
   const [form] = Form.useForm();
   return (
     <Modal
       className="flex flex-col"
-      title={<div className="flex w-full justify-center"><span className="font-black text-lg">Editar Nomenclador</span></div>}
+      title={
+        <div className="flex w-full justify-center">
+          <span className="font-black text-lg">Editar Nomenclador</span>
+        </div>
+      }
       centered
       open={open}
       style={{ textAlign: "left" }}
       destroyOnClose
-      onOk={() => {
-        form
-          .validateFields()
-          .then((values) => {
-            onCreate(values);
-            form.resetFields();
-          })
-          .catch((error) => {
-            console.log("Validate Failed:", error);
-          });
-      }}
       onCancel={onCancel}
       okType="default"
       okText="Editar"
       cancelText="Cancelar"
+      footer={[
+        <div key="footer" className="flex gap-2 w-full justify-end">
+          <button
+            key="2"
+            className="bg-danger-500 cursor-pointer hover:bg-danger-600 ease-in-out duration-300 w-[5rem] h-[2rem] flex items-center p-1 text-sm font-bold text-white-100  justify-center gap-2 rounded-md"
+            onClick={onCancel}
+          >
+            Cancelar
+          </button>
+          <button
+            key="1"
+            className="bg-success-500 cursor-pointer hover:bg-success-600 ease-in-out duration-300 w-[5rem] h-[2rem] flex items-center p-1 text-sm font-bold text-white-100  justify-center gap-2 rounded-md "
+            onClick={() => {
+              form
+                .validateFields()
+                .then((values) => {
+                  onCreate(values);
+                  console.log("🚀 ~ file: CreateNomenclatorForm.tsx:51 ~ .then ~ values:", values);
+                  form.resetFields();
+                })
+                .catch((error) => {
+                  console.log("Validate Failed:", error);
+                });
+            }}
+          >
+            Editar
+          </button>
+        </div>,
+      ]}
     >
       <Form
         form={form}
@@ -75,7 +96,6 @@ export const EditNomenclatorForm: React.FC<CollectionCreateFormProps> = ({ open,
             name: "code",
             value: defaultValues?.code,
           },
-          
         ]}
       >
         <Form.Item name="category" label="Categoría" rules={[{ required: true, message: "Campo requerido" }]}>
@@ -84,7 +104,6 @@ export const EditNomenclatorForm: React.FC<CollectionCreateFormProps> = ({ open,
         <Form.Item name="code" label="Código" rules={[{ required: true, message: "Campo requerido" }]}>
           <Input />
         </Form.Item>
-
       </Form>
     </Modal>
   );
