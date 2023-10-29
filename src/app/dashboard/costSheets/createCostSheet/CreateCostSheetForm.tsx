4b593508813f97ee3c207e20e5d1b91ef1aff229
Daniel Muniz <danielnubiz902@gmail.com>
2023-10-28 23:05:54 -0400

    Fix styles

--- src/app/dashboard/costSheets/createCostSheet/CreateCostSheetForm.tsx
+++ src/app/dashboard/costSheets/createCostSheet/CreateCostSheetForm.tsx
@@ -18,52 +18,57 @@ export const CreateCostSheetForm = () => {
       name="createCostSheetForm"
       labelCol={{ span: 0 }}
       wrapperCol={{ span: 0 }}
-      className="w-[80%]"
+      className="w-full flex flex-col gap-0"
       initialValues={{ remember: true }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
       autoComplete="off"
       requiredMark={"optional"}
-      size="large"
+      size="middle"
     >
-      <Form.Item<ICostSheet> label={<span className="font-bold text-md">Tarea a ejecutar</span>} name="taskName" rules={[{ required: true, message: "Introduzca el nombre de la tarea" }]}>
+      <Form.Item className="mb-1" label={<span className="font-bold text-md">Tarea a ejecutar</span>} name="taskName" rules={[{ required: true, message: "Introduzca el nombre de la tarea" }]}>
         <Input />
       </Form.Item>
-      <Form.Item<ICostSheet> label={<span className="font-bold text-md">Cantidad de empleados</span>} name="workersAmount" rules={[{ required: true, message: "Introduzca el nombre de la tarea" }]}>
+      <Form.Item
+        className="mb-1"
+        label={<span className="font-bold text-md">Cantidad de empleados</span>}
+        name="workersAmount"
+        rules={[{ required: true, message: "Introduzca el nombre de la tarea" }]}
+      >
         <InputNumber className="w-full" />
       </Form.Item>
-      <section>
-        <label className="text-md font-bold my-5" htmlFor="rawMaterials">
+      <section className=" flex flex-col w-full">
+        <label className="text-md font-bold mb-3" htmlFor="rawMaterials">
           Materias Primas Fundamentales
         </label>
         <Form.List name="rawMaterials">
           {(fields, { add, remove }) => (
-            <>
+            <div className="flex flex-col w-full">
               {fields.map(({ key, name, ...restField }) => (
-                <div key={key} className="flex items-center pt-5" >
-                  <div className="flex items-center content-center h-10">
-                  <Form.Item  {...restField} name={[name, "description"]} rules={[{ required: true, message: "Introduzca la descripción" }]}>
-                    <Input placeholder="Descripción" className="w-[40rem]" />
-                  </Form.Item>
-                  <Form.Item {...restField} name={[name, "unitMeasure"]} rules={[{ required: true, message: "Introduzca la unidad de medida" }]}>
-                    <Input placeholder="Unidad de Medida" className="w-full" />
-                  </Form.Item>
-                  <Form.Item {...restField} name={[name, "amount"]} rules={[{ required: true, message: "Introduzca la cantidad" }]}>
-                    <InputNumber placeholder="Cantidad" className="w-full" />
-                  </Form.Item>
-                  <Form.Item {...restField} name={[name, "price"]} rules={[{ required: true, message: "Introduzca el precio" }]}>
-                    <Input placeholder="Precio" className="w-full" />
-                  </Form.Item>
-                  <MinusCircleOutlined onClick={() => remove(name)}  />
+                <div key={key} className="w-full">
+                  <div className="flex items-center flex-row mb-0 h-9 w-full">
+                    <Form.Item className="w-full" {...restField} name={[name, "description"]} rules={[{ required: true, message: "Introduzca la descripción" }]}>
+                      <Input placeholder="Descripción" className="w-full" />
+                    </Form.Item>
+                    <Form.Item {...restField} name={[name, "unitMeasure"]} rules={[{ required: true, message: "Introduzca la unidad de medida" }]}>
+                      <Input placeholder="Unidad de Medida" className="w-full" />
+                    </Form.Item>
+                    <Form.Item {...restField} name={[name, "amount"]} rules={[{ required: true, message: "Introduzca la cantidad" }]}>
+                      <InputNumber placeholder="Cantidad" className="w-full" />
+                    </Form.Item>
+                    <Form.Item {...restField} name={[name, "price"]} rules={[{ required: true, message: "Introduzca el precio" }]}>
+                      <Input placeholder="Precio" className="w-full" />
+                    </Form.Item>
+                    <MinusCircleOutlined className="mb-auto" onClick={() => remove(name)} />
                   </div>
                 </div>
               ))}
               <Form.Item>
-                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                <Button className="flex flex-row justify-center items-center " type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
                   Add field
                 </Button>
               </Form.Item>
-            </>
+            </div>
           )}
         </Form.List>
       </section>
