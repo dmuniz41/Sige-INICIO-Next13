bb8ab513959f2b9b4ac12075378dde516cafcb25
Daniel Muniz <danielnubiz902@gmail.com>
2023-10-30 19:57:41 -0400

    Add new inputs to create cost sheet form

--- src/app/dashboard/costSheets/createCostSheet/CreateCostSheetForm.tsx
+++ src/app/dashboard/costSheets/createCostSheet/CreateCostSheetForm.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { ICostSheet } from "@/models/costSheet";
-import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import { Button, Space, Form, Input, InputNumber } from "antd";
+
+import { Form, Input, InputNumber } from "antd";
 import React from "react";
 import { CSFormSection } from "./CSFormSection";
 
@@ -27,18 +26,31 @@ export const CreateCostSheetForm = () => {
       requiredMark={"optional"}
       size="middle"
     >
-      <Form.Item className="mb-1" label={<span className="font-bold text-md">Tarea a ejecutar</span>} name="taskName" rules={[{ required: true, message: "Introduzca el nombre de la tarea" }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item
-        className="mb-1"
-        label={<span className="font-bold text-md">Cantidad de empleados</span>}
-        name="workersAmount"
-        rules={[{ required: true, message: "Introduzca el nombre de la tarea" }]}
-      >
-        <InputNumber className="w-full" />
-      </Form.Item>
-      
+      <section className="w-full flex-col gap-2 justify-start">
+        <Form.Item className="mb-3 " label={<span className="font-bold text-md">Tarea a ejecutar</span>} name="taskName" rules={[{ required: true, message: "Campo requerido" }]}>
+          <Input className="w-[30rem]" />
+        </Form.Item>
+        <div className="flex w-full gap-2 justify-start">
+          <Form.Item className="mb-3 " label={<span className="font-bold text-md">Cantidad de empleados</span>} name="workersAmount" rules={[{ required: true, message: "Campo requerido" }]}>
+            <InputNumber className="w-[5rem]" />
+          </Form.Item>
+          <Form.Item className="mb-3 " label={<span className="font-bold text-md">Talento artístico</span>} name="artisticTalent" rules={[{ required: true, message: "Campo requerido" }]}>
+            <InputNumber className="w-[5rem] " />
+          </Form.Item>
+          <Form.Item className="mb-3 " label={<span className="font-bold text-md">Utilidad</span>} name="representationCost" rules={[{ required: true, message: "Campo requerido" }]}>
+            <InputNumber className="w-[5rem] " />
+          </Form.Item>
+          <Form.Item
+            className="mb-3 "
+            label={<span className="font-bold text-md">Materias primas aportadas por el cliente</span>}
+            name="rawMaterialsByClient"
+            rules={[{ required: true, message: "Campo requerido" }]}
+          >
+            <InputNumber className="w-[5rem] " />
+          </Form.Item>
+        </div>
+      </section>
+
       <CSFormSection label="Materias primas fundamentales " name="rawMaterials" />
       <CSFormSection label="Salarios directos" name="directSalaries" />
       <CSFormSection label="Otros gastos directos" name="otherDirectExpenses" />
