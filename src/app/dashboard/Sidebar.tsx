e993048ed36b3a7ce50a0e34311acdaf5e0150a8
Daniel Muniz <danielnubiz902@gmail.com>
2024-01-11 10:12:06 -0500

    Add service fees to sidebar

--- src/app/dashboard/Sidebar.tsx
+++ src/app/dashboard/Sidebar.tsx
@@ -115,6 +115,23 @@ const items: MenuProps["items"] = [
     ),
   },
   {
+    label: <Link href="/dashboard/serviceFees">Tarifas de Servicios</Link>,
+    key: "serviceFees",
+    icon: (
+      <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 3m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+        <path d="M8 7m0 1a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1z" />
+        <path d="M8 14l0 .01" />
+        <path d="M12 14l0 .01" />
+        <path d="M16 14l0 .01" />
+        <path d="M8 17l0 .01" />
+        <path d="M12 17l0 .01" />
+        <path d="M16 17l0 .01" />
+      </svg>
+    ),
+  },
+  {
     label: <Link href="/dashboard/nomenclators">Nomencladores</Link>,
     key: "nomenclator",
     icon: (
