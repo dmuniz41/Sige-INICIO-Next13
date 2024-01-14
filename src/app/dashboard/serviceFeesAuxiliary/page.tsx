7490f510cba6bffa3a49db1d51f19e45f6afef9a
Daniel Muniz <danielnubiz902@gmail.com>
2024-01-13 19:33:23 -0500

    Add new routes and no data svg

---
+++ src/app/dashboard/serviceFeesAuxiliary/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div  className="flex w-full min-h-[90%] pt-[3rem] pl-[300px] pr-[3rem] overflow-hidden">
+      <div
+        className="w-[80%] items-start justify-start flex-col gap-2 h-full p-4 animate-fade animate-once animate-duration-150 grow overflow-auto"
+      >
+        <h1>Auxiliares</h1>
+      </div>
+    </div>
+  );
+}
