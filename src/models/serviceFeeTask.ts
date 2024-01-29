39cafc302f230aadc20caca73cd5ec1bfceb9cf4
Daniel Muniz <danielnubiz902@gmail.com>
2024-01-28 19:22:21 -0500

    Add endpoints for service fee tasks

--- src/models/serviceFeeTask.ts
+++ src/models/serviceFeeTask.ts
@@ -1,40 +1,44 @@
 import { Schema, Model, model, models } from "mongoose";
 
 export interface IServiceFeeTask {
+  _id: string;
   amount: number;
   category: string;
   description: string;
   price: number;
   unitMeasure: string;
-  value: number;
+  key?: string;
 
-  complexity: {
-    levels: [
-      {
-        name: "Alta";
-        coefficient: number;
-      },
-      {
-        name: "Media";
-        coefficient: number;
-      },
-      {
-        name: "Baja";
-        coefficient: number;
-      }
-    ];
-  };
+  complexityLevels: [
+    {
+      name: "Alta";
+      coefficient: number;
+    },
+    {
+      name: "Media";
+      coefficient: number;
+    },
+    {
+      name: "Baja";
+      coefficient: number;
+    }
+  ];
 }
 
 export const ServiceFeeTaskSchema = new Schema<IServiceFeeTask, Model<IServiceFeeTask>>({
   amount: {
     type: Number,
   },
+  key: {
+    type: String,
+  },
   category: {
     type: String,
   },
   description: {
     type: String,
+    required: true,
+    unique: true,
   },
   price: {
     type: Number,
@@ -42,9 +46,20 @@ export const ServiceFeeTaskSchema = new Schema<IServiceFeeTask, Model<IServiceFe
   unitMeasure: {
     type: String,
   },
-  value: {
-    type: Number,
-  },
+  complexityLevels: [
+    {
+      name: String,
+      coefficient: Number,
+    },
+    {
+      name: String,
+      coefficient: Number,
+    },
+    {
+      name: String,
+      coefficient: Number,
+    },
+  ],
 });
 
 const ServiceFeeTask = models.ServiceFeeTask || model("ServiceFeeTask", ServiceFeeTaskSchema);
