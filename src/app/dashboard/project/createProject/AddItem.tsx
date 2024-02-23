3286719437d2cf2ee44b9480b5c3061fb04586a4
Daniel Muniz <danielnubiz902@gmail.com>
2024-02-23 11:28:19 -0500

    Add new options to project table

---
+++ src/app/dashboard/project/createProject/AddItem.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Form, Input, InputNumber, Modal} from "antd";
+import { IItem } from "@/models/project";
+
+interface CollectionCreateFormProps {
+  open: boolean;
+  onCreate: (values: IItem) => void;
+  onCancel: () => void;
+}
+
+export const AddItemModal: React.FC<CollectionCreateFormProps> = ({ open, onCreate, onCancel }) => {
+  const [form] = Form.useForm();
+  return (
+    <Modal
+      className="flex flex-col"
+      title={
+        <div className="flex w-full justify-center">
+          <span className="font-bold text-lg">Nuevo Item</span>
+        </div>
+      }
+      style={{ textAlign: "left" }}
+      centered
+      open={open}
+      destroyOnClose
+      onCancel={onCancel}
+      okType="default"
+      okText="Crear"
+      width={"600px"}
+      cancelText="Cancelar"
+      footer={[
+        <div key="footer" className="flex gap-2 w-full justify-end">
+          <button key="2" className="modal-btn-danger" onClick={onCancel}>
+            Cancelar
+          </button>
+          <button
+            key="1"
+            className="modal-btn-primary "
+            onClick={() => {
+              form
+                .validateFields()
+                .then((values) => {
+                  onCreate(values);
+                  form.resetFields();
+                })
+                .catch((error) => {
+                  console.log("Validate Failed:", error);
+                });
+            }}
+          >
+            Añadir
+          </button>
+        </div>,
+      ]}
+    >
+      <Form form={form} layout="horizontal" name="addItem" size="middle">
+        <Form.Item name="idNumber" label="Número" className="w-[10rem]" rules={[{ required: true, message: "Campo requerido" }]}>
+          <InputNumber />
+        </Form.Item>
+        <Form.Item name="description" label="Descripción" rules={[{ required: true, message: "Campo requerido" }]}>
+          <Input/>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
---
+++ src/app/global/Loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+export const Loading = () => {
+  return (
+    <div className="w-full h-full bg-primary-300">
+      <h1 className="font-bold text-xl">LOADING!!!!</h1>
+    </div>
+  );
+};
