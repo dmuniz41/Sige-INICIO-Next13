8d615177f7c911274fdd9224044c173d4f25de38
Daniel Muniz <danielnubiz902@gmail.com>
2024-04-02 16:05:08 -0400

    Add offer PDF report

--- src/helpers/OfferPDFReport.js
+++ src/helpers/OfferPDFReport.js
@@ -27,17 +27,16 @@ const styles = StyleSheet.create({
     marginBottom: 5
   },
   clientInfoElementHeader: {
-    fontSize: "12pt",
+    fontSize: "10pt",
     fontWeight: "bold",
     marginRight: "2px",
     fontFamily: "Arial"
   },
   clientInfoElementText: {
-    fontSize: "12pt"
+    fontSize: "10pt"
   },
 
   // ? ESTILOS DE LA SECCION DE LA LISTA DE ITEMS //
-
   offerNameContainer: {
     borderStyle: BORDER_STYLE,
     borderColor: BORDER_COLOR,
@@ -49,7 +48,8 @@ const styles = StyleSheet.create({
     width: "100%",
     borderStyle: BORDER_STYLE,
     borderColor: BORDER_COLOR,
-    borderRightWidth: 0,
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
     borderTopWidth: 0,
     borderBottomWidth: "1px",
     display: "flex",
@@ -70,13 +70,13 @@ const styles = StyleSheet.create({
   },
   tableCellHeader: {
     margin: 2,
-    fontSize: "12pt",
+    fontSize: "10pt",
     fontWeight: "bold",
     fontFamily: "Arial"
   },
   tableCell: {
     margin: 2,
-    fontSize: "12pt"
+    fontSize: "10pt"
   },
   textCenter: {
     textAlign: "center"
@@ -89,6 +89,51 @@ function checkStrEmpty(str) {
 
 export default function OfferPDFReport(props) {
   const { clientInfo, data, title, totalValue } = props;
+
+  const fields = [
+    {
+      title: "Descripción",
+      custom: true,
+      component: (item) => `${item.description}`,
+      width: "50"
+    },
+    {
+      title: "U/M",
+      custom: true,
+      component: (item) => `${item.unitMeasure}`,
+      width: "15"
+    },
+    {
+      title: "Cant.",
+      custom: true,
+      component: (item) =>
+        `${item.amount.toLocaleString("DE", {
+          maximumFractionDigits: 2,
+          minimumFractionDigits: 2
+        })}`,
+      width: "10"
+    },
+    {
+      title: "Precio CUP",
+      custom: true,
+      component: (item) =>
+        `$ ${item.price.toLocaleString("DE", {
+          maximumFractionDigits: 2,
+          minimumFractionDigits: 2
+        })}`,
+      width: "15"
+    },
+    {
+      title: "Importe CUP",
+      custom: true,
+      component: (item) =>
+        `$ ${item.value.toLocaleString("DE", {
+          maximumFractionDigits: 2,
+          minimumFractionDigits: 2
+        })}`,
+      width: "10"
+    }
+  ];
   return (
     <Document>
       <Page wrap orientation="landscape" size={"LETTER"} style={styles.body}>
@@ -117,96 +162,141 @@ export default function OfferPDFReport(props) {
             width: "100%",
             borderStyle: BORDER_STYLE,
             borderColor: BORDER_COLOR,
-            borderLeftWidth: "1px",
-            borderRightWidth: "1px",
             borderTopWidth: "1px",
-            borderBottomWidth: "0px",
             display: "flex",
             flexDirection: "column"
           }}
         >
-          <View style={styles.offerNameContainer}>
-            <View
-              style={{ borderBottomWidth: "1px", borderBottomStyle: "solid", paddingHorizontal: 4 }}
-            >
-              <Text style={styles.clientInfoElementHeader}>{title}</Text>
-            </View>
-            {/* LISTA DE ITEMS */}
-            {data.map((item, index) => {
-              return (
-                <View key={index} style={styles.itemContainer}>
-                  {/* NUMERO DE ITEM */}
-                  <View
+          <View
+            style={{
+              borderBottomWidth: "1px",
+              borderStyle: "solid",
+              paddingHorizontal: 4,
+              borderLeftWidth: 1,
+              borderRightWidth: 1
+            }}
+          >
+            <Text style={styles.clientInfoElementHeader}>{title}</Text>
+          </View>
+          {/* LISTA DE ITEMS */}
+          {data.map((item, index) => {
+            return (
+              <View wrap={false} key={index} style={styles.itemContainer}>
+                {/* NUMERO DE ITEM */}
+                <View
+                  style={{
+                    backgroundColor: "#cccccc",
+                    width: "25px",
+                    justifyContent: "center",
+                    display: "flex"
+                  }}
+                >
+                  <Text
                     style={{
-                      backgroundColor: "#f4f6f9",
-                      width: "15px",
-                      borderRightWidth: "1px",
-                      borderRightStyle: "solid",
-                      justifyContent: "center",
-                      display: "flex"
+                      fontSize: 10,
+                      fontFamily: "Arial",
+                      fontWeight: "bold",
+                      textAlign: "center"
                     }}
                   >
-                    <Text
+                    {index + 1}
+                  </Text>
+                </View>
+                {/* DESCRIPCION, LISTA DE ACTIVIDADES Y VALOR DEL ITEM */}
+                <View style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+                  {/* DESCRIPCION Y VALOR DEL ITEM */}
+                  <View style={{ display: "flex", flexDirection: "row" }}>
+                    <View
                       style={{
-                        fontSize: 10,
-                        fontFamily: "Arial",
-                        fontWeight: "bold",
-                        textAlign: "center"
+                        display: "flex",
+                        backgroundColor: "#cccccc",
+                        borderLeftStyle: "solid",
+                        borderLeftWidth: "1px",
+                        borderStyle: "solid",
+                        width: "90%",
+                        padding: 2
                       }}
                     >
-                      {index + 1}
-                    </Text>
-                  </View>
-                  {/* DESCRIPCION, LISTA DE ACTIVIDADES Y VALOR DEL ITEM */}
-                  <View style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-                    {/* DESCRIPCION Y VALOR DEL ITEM */}
-                    <View style={{ display: "flex", flexDirection: "row" }}>
-                      <View
-                        wrap
-                        style={{
-                          display: "flex",
-                          backgroundColor: "#f4f6f9",
-                          borderRightWidth: "1px",
-                          borderStyle: "solid",
-                          width: "90%",
-                          padding: 2
-                        }}
-                      >
-                        <Text style={{ fontFamily: "Arial", fontWeight: "bold", fontSize: "10pt" }}>
-                          {item.description}
-                        </Text>
-                      </View>
-                      <View
-                        style={{
-                          display: "flex",
-                          backgroundColor: "#f4f6f9",
-                          alignItems: "center",
-                          width: "80px"
-                        }}
-                      >
-                        <Text
-                          style={{
-                            fontFamily: "Arial",
-                            fontWeight: "bold",
-                            textAlign: "center",
-                            fontSize: 10
-                          }}
-                        >
-                          ${" "}
-                          {item.value.toLocaleString("DE", {
-                            maximumFractionDigits: 2,
-                            minimumFractionDigits: 2
-                          })}
-                        </Text>
-                      </View>
+                      <Text style={{ fontFamily: "Arial", fontWeight: "bold", fontSize: "10pt" }}>
+                        {item.description}
+                      </Text>
                     </View>
-                    {/* LISTA DE ACTIVIDADES */}
-                    <View>TABLA DE ACTIVIDADES</View>
+                    <View
+                      style={{
+                        display: "flex",
+                        backgroundColor: "#cccccc",
+                        alignItems: "center",
+                        flexGrow: 1,
+                        borderLeftStyle: "solid",
+                        borderLeftWidth: "1px"
+                      }}
+                    >
+                      <Text
+                        style={{
+                          fontFamily: "Arial",
+                          fontWeight: "bold",
+                          textAlign: "center",
+                          fontSize: 10
+                        }}
+                      >
+                        ${" "}
+                        {item.value.toLocaleString("DE", {
+                          maximumFractionDigits: 2,
+                          minimumFractionDigits: 2
+                        })}
+                      </Text>
+                    </View>
                   </View>
+                  {/* LISTA DE ACTIVIDADES */}
+                  <CustomTablePDF fields={fields} data={item.activities} />
                 </View>
-              );
-            })}
-            <View></View>
+              </View>
+            );
+          })}
+          <View style={{ display: "flex", flexDirection: "row" }}>
+            <View
+              style={{
+                display: "flex",
+                backgroundColor: "#cccccc",
+                borderLeftStyle: "solid",
+                borderLeftWidth: 1,
+                borderRightWidth: 1,
+                borderBottomWidth: 1,
+                borderStyle: "solid",
+                width: "90.5%",
+                padding: 2
+              }}
+            >
+              <Text style={{ fontFamily: "Arial", fontWeight: "bold", fontSize: "10pt" }}>
+                VALOR TOTAL
+              </Text>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                backgroundColor: "#cccccc",
+                alignItems: "center",
+                flexGrow: 1,
+                borderLeftStyle: "solid",
+                borderRightWidth: 1,
+                borderBottomWidth: 1
+              }}
+            >
+              <Text
+                style={{
+                  fontFamily: "Arial",
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  fontSize: 10
+                }}
+              >
+                ${" "}
+                {totalValue?.toLocaleString("DE", {
+                  maximumFractionDigits: 2,
+                  minimumFractionDigits: 2
+                })}
+              </Text>
+            </View>
           </View>
         </View>
       </Page>
@@ -214,56 +304,54 @@ export default function OfferPDFReport(props) {
   );
 }
 
-// const CustomTablePDF = (props) => {
-//   const { fields = [], data = [] } = props;
-//   let tableCol = {
-//     borderStyle: BORDER_STYLE,
-//     borderColor: BORDER_COLOR,
-//     borderBottomColor: "#000",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     borderLeft: "1px",
-//     borderBottom: "1px"
-//   };
-//   return (
-//     <View style={styles.subsectionTable}>
-//       <View style={[styles.tableRow, styles.headerBg]}>
-//         {fields.map((_item, _idx) => (
-//           <View key={_idx} style={[tableCol, { width: _item.width + "%" }]}>
-//             <Text style={[styles.tableCellHeader, { textAlign: "center" }]}>{_item.title}</Text>
-//           </View>
-//         ))}
-//       </View>
+const CustomTablePDF = (props) => {
+  const { fields = [], data = [] } = props;
+  let tableCol = {
+    borderStyle: BORDER_STYLE,
+    borderColor: BORDER_COLOR,
+    borderLeft: "1px",
+    borderBottom: "0px",
+    borderTopWidth: "1px"
+  };
+  return (
+    <View style={styles.subsectionTable}>
+      <View style={[styles.tableRow, styles.headerBg]}>
+        {fields.map((_item, _idx) => (
+          <View key={_idx} style={[tableCol, { width: _item.width + "%" }]}>
+            <Text style={[styles.tableCellHeader, { textAlign: "center" }]}>{_item.title}</Text>
+          </View>
+        ))}
+      </View>
 
-//       {data.map(
-//         (item, idx) =>
-//           item && (
-//             <View key={idx} style={styles.tableRow}>
-//               {fields.map((_item, _idx) => {
-//                 let val = item[_item.value] || "";
-//                 let value_alt = (_item.value_alt && item[_item.value_alt]) || "";
+      {data.map(
+        (item, idx) =>
+          item && (
+            <View key={idx} style={styles.tableRow}>
+              {fields.map((_item, _idx) => {
+                let val = item[_item.value] || "";
+                let value_alt = (_item.value_alt && item[_item.value_alt]) || "";
 
-//                 if (_item.custom) {
-//                   return (
-//                     <View key={_idx} style={[tableCol, { width: _item.width + "%" }]}>
-//                       <Text style={[styles.tableCell, item.style ? item.style : {}]}>
-//                         {_item.component(item)}
-//                       </Text>
-//                     </View>
-//                   );
-//                 } else {
-//                   return (
-//                     <View key={_idx} style={[styles.tableCol, { width: _item.width + "%" }]}>
-//                       <Text style={[styles.tableCell, item.style ? item.style : {}]}>
-//                         {checkStrEmpty(val) ? value_alt : val || "-"}
-//                       </Text>
-//                     </View>
-//                   );
-//                 }
-//               })}
-//             </View>
-//           )
-//       )}
-//     </View>
-//   );
-// };
+                if (_item.custom) {
+                  return (
+                    <View key={_idx} style={[tableCol, { width: _item.width + "%" }]}>
+                      <Text style={[styles.tableCell, item.style ? item.style : {}]}>
+                        {_item.component(item)}
+                      </Text>
+                    </View>
+                  );
+                } else {
+                  return (
+                    <View key={_idx} style={[styles.tableCol, { width: _item.width + "%" }]}>
+                      <Text style={[styles.tableCell, item.style ? item.style : {}]}>
+                        {checkStrEmpty(val) ? value_alt : val || "-"}
+                      </Text>
+                    </View>
+                  );
+                }
+              })}
+            </View>
+          )
+      )}
+    </View>
+  );
+};
