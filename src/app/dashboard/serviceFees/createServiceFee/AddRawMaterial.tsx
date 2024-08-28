8f75361a8bd4811b4e125e76a51a59f54375a3da
daniel.muniz <daniel.muniz@etecsa.cu>
2024-08-28 09:47:09 -0500

    refactor: :ambulance: Fix the prices for the materials when add new raw material in a service fee

--- src/app/dashboard/serviceFees/createServiceFee/AddRawMaterial.tsx
+++ src/app/dashboard/serviceFees/createServiceFee/AddRawMaterial.tsx
@@ -22,14 +22,9 @@ export const AddRawMaterialModal: React.FC<CollectionCreateFormProps> = ({ open,
   const [currentUnitMeasure, setCurrentUnitMeasure] = useState<string>("");
   const [rawMaterialValue, setRawMaterialValue] = useState<number>(0);
   const { materials }: { materials: IMaterial[] } = useAppSelector((state: RootState) => state?.material);
-  
-  console.log("🚀 ~ currentPrice:", currentPrice)
-  
   const { nomenclators, materialsNomenclators }: { nomenclators: INomenclator[]; materialsNomenclators: IMaterialNomenclator[] } =
     useAppSelector((state: RootState) => state?.nomenclator);
-
   const { serviceFeeAuxiliary }: { serviceFeeAuxiliary: IServiceFeeAuxiliary } = useAppSelector((state: RootState) => state?.serviceFee);
-
   const DBMaterials: INomenclator[] = [];
   const dispatch = useAppDispatch();
 
@@ -111,31 +106,33 @@ export const AddRawMaterialModal: React.FC<CollectionCreateFormProps> = ({ open,
             style={{ width: "100%" }}
             options={listOfMaterials}
             onSelect={(value) => {
-              const selectedMaterial = materials.find(
+              const warehouseMaterial = materials.find(
                 (material: IMaterial) =>
                   `${material.category} ${material.materialName}`.trim().toLowerCase() === String(value.label).trim().toLowerCase()
               );
-              
-              console.log("🚀 ~ materials:", materials)
-              console.log("🚀 ~ selectedMaterial:", selectedMaterial);
 
+              // ? BUSCA EL MATERIAL EN LOS NOMENCLADORES DE MATERIALES PARA SABER SI ES GASTABLE
               const materialNomenclator = materialsNomenclators.find(
-                (mn) => mn.name.trim().toLocaleLowerCase() === selectedMaterial?.category.trim().toLocaleLowerCase()
+                (mn) => mn.name.trim().toLocaleLowerCase() === warehouseMaterial?.category.trim().toLocaleLowerCase()
               );
-              setCurrentUnitMeasure(selectedMaterial?.unitMeasure!);
+              setCurrentUnitMeasure(warehouseMaterial?.unitMeasure!);
+
+              // ? BUSCA EN LOS NOMENCALDORES DE MATERIALES EL MATERIAL SELECIONADO PARA OBTERNER EL PRECIO MAYOR
+              const nomenclatorMaterial = DBMaterials.find(
+                (DBMaterial) => DBMaterial.code.trim().toLowerCase() === String(value.label).trim().toLowerCase()
+              );
 
               // ? SI EL MATERIAL ES GASTABLE SE LE APLICA EL COEFICIENTE DE MERMA AL PRECIO DEL MATERIAL, EN CASO CONTRARIO MANTIENE EL PRECIO ORIGINAL?//
               if (materialNomenclator?.isDecrease) {
-                setCurrentPrice(selectedMaterial?.costPerUnit! * serviceFeeAuxiliary?.mermaCoefficient);
+                setCurrentPrice(nomenclatorMaterial?.value! * serviceFeeAuxiliary?.mermaCoefficient);
               } else {
-                setCurrentPrice(selectedMaterial?.costPerUnit!);
+                setCurrentPrice(nomenclatorMaterial?.value!);
               }
               setRawMaterialValue(0);
 
               form.setFieldsValue({
-                unitMeasure: selectedMaterial?.unitMeasure,
-                price: form.getFieldValue("description")?.value,
-                amount: 0
+                unitMeasure: warehouseMaterial?.unitMeasure,
+                amount: 1
               });
             }}
             showSearch
@@ -148,7 +145,7 @@ export const AddRawMaterialModal: React.FC<CollectionCreateFormProps> = ({ open,
         </Form.Item>
         <Form.Item name="amount" label="Cantidad" className="w-[10rem]" rules={[{ required: true, message: "Campo requerido" }]}>
           <InputNumber
-            min={0}
+            min={1}
             onChange={(value: number | null) => {
               setRawMaterialValue(value! * currentPrice);
             }}
