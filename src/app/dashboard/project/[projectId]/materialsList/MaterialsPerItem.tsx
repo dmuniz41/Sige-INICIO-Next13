80be2388796eda8fb6f6f82eecc6a7fac89dd6df
Daniel Muniz <danielnubiz902@gmail.com>
2024-12-23 14:09:02 -0500

    build: :arrow_up: Add react query for data feching

--- src/app/dashboard/project/[projectId]/materialsList/MaterialsPerItem.tsx
+++ src/app/dashboard/project/[projectId]/materialsList/MaterialsPerItem.tsx
@@ -5,20 +5,28 @@ import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
 import { IProject } from "@/models/project";
 import { RootState } from "@/store/store";
 import { loadSelectedOffer } from "@/actions/offer";
-import { startLoadSelectedProject } from "@/actions/project";
 import { IOffer } from "@/models/offer";
 
-export const MaterialsPerItem = (props: { projectId: string }) => {
-  const { projectId } = props;
+export const MaterialsPerItem = () => {
   const dispatch = useAppDispatch();
 
+  
   const { selectedProject }: { selectedProject: IProject } = useAppSelector((state: RootState) => state?.project);
-
+  const { selectedOffer }: { selectedOffer: IOffer } = useAppSelector((state: RootState) => state?.offer);
+  
   useEffect(() => {
     dispatch(loadSelectedOffer(selectedProject.finalOfferId));
-  }, [dispatch]);
+  }, [selectedProject,dispatch]);
 
-  const { selectedOffer }: { selectedOffer: IOffer } = useAppSelector((state: RootState) => state?.offer);
 
-  return <>SECTION {JSON.stringify(selectedOffer?.materialsList)}</>;
+  const newArray = selectedOffer?.materialsList?.filter((material) => selectedOffer?.itemsList.some((item) => item.key === material.itemId))
+    .map((material) => {
+      const matchingItem = selectedOffer?.itemsList.find((item) => item.key === material.itemId);
+      return { ...material, ItemDescription: matchingItem?.description };
+    });
+
+  return <>
+  <br />
+  NEW ARRAY {JSON.stringify(newArray)}
+  </>;
 };
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -3,11 +3,17 @@
 import { store } from "../store/store";
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
   return (
     <Provider store={store}>
-      <SessionProvider>{children}</SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider>{children}</SessionProvider>
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     </Provider>
   );
 }
