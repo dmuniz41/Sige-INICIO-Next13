a2c8ce9f79de6a0a3dbdc7f3bd1485827134457a
Daniel Muniz <danielnubiz902@gmail.com>
2024-12-27 10:31:54 -0500

    refactor: :recycle: Adding code to disaggregation mterials screen

--- src/app/dashboard/project/[projectId]/materialsList/MaterialsPerItem.tsx
+++ src/app/dashboard/project/[projectId]/materialsList/MaterialsPerItem.tsx
@@ -1,12 +1,24 @@
 "use client";
-import React, { useState } from "react";
+import { useParams } from "next/navigation";
+import React, { useEffect } from "react";
 
-import { useAppSelector } from "@/hooks/hooks";
 import { IProject } from "@/models/project";
 import { RootState } from "@/store/store";
+import { startLoadSelectedProject } from "@/actions/project";
+import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
+import Loading from "./loading";
 import useDisaggregationByMaterialsAPI from "@/hooks/offers/useDisaggregationByMaterials";
 
 export const MaterialsPerItem = () => {
+  const { projectId }: { projectId: string } = useParams();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (projectId) {
+      dispatch(startLoadSelectedProject(projectId));
+    }
+  }, [dispatch, projectId]);
+
   const { selectedProject }: { selectedProject: IProject } = useAppSelector(
     (state: RootState) => state?.project
   );
@@ -14,6 +26,17 @@ export const MaterialsPerItem = () => {
     selectedProject.finalOfferId
   );
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!data) {
+    return <div>No data available.</div>;
+  }
   return (
     <>
       <br />
---
+++ src/app/dashboard/project/[projectId]/materialsList/loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div>
+      <h1>Loading...</h1>
+      {/* You can add a spinner or any loading animation here */}
+    </div>
+  );
+};
+
+export default Loading;
