91bf1fea9a8a2f03b5acc8eaf3338b1d1293cdbc
Daniel Muniz <danielnubiz902@gmail.com>
2025-01-01 20:29:43 -0500

    Adding styles to new disaggregation page

--- src/app/dashboard/project/[projectId]/materialsList/ItemSection.tsx
+++ src/app/dashboard/project/[projectId]/materialsList/ItemSection.tsx
@@ -1,19 +1,33 @@
 import Title from "antd/es/typography/Title";
 import { ActivitiesSection } from "./ActivitiesSection";
 
+interface IItemsListProps {
+  itemId: string;
+  itemDescription: string;
+  activities: {
+    description: string;
+    amount: number;
+    materials: any[];
+  }[];
+}
+
 export const ItemSection = ({
-  item
+  item,
+  index
 }: {
-  item: {
-    itemId: string;
-    itemDescription: string;
-    activities: { description: string; amount: number; materials: any[] }[];
-  };
+  item: IItemsListProps;
+  index: number;
 }) => {
   return (
-    <section className=" mb-2 rounded-md p-4">
-      <Title level={3}>{item.itemDescription}</Title>
-      <div className="bg-dark h-[1px] mb-2"></div>
+    <section className="mb-2 border border-solid p-4 border-background_light rounded-md shadow-md">
+      <div className="">
+        <div className="text-2xl mb-2 font-bold">
+          <span>
+            No.{index + 1}: {item.itemDescription}
+          </span>
+        </div>
+        <div className="bg-dark h-[1px] mb-1"></div>
+      </div>
       <ActivitiesSection activities={item.activities} />
     </section>
   );
