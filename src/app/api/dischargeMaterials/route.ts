37105f687969837ba9cb0763f7bd533c8efafeb9
Daniel Muniz <danielnubiz902@gmail.com>
2025-01-17 23:07:52 -0500

    feat: :sparkles: Adding endpoint to update the discharge materials

--- src/app/api/dischargeMaterials/route.ts
+++ src/app/api/dischargeMaterials/route.ts
@@ -1,10 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
 import { verifyJWT } from "@/libs/jwt";
 import { connectDB } from "@/libs/mongodb";
 import DischargeMaterials, {
+  IDischargeMaterials,
   IDischargeMaterialsList
 } from "@/models/dischargeMaterials";
 import Offer from "@/models/offer";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const { offerId }: { offerId: string } = await request.json();
@@ -65,6 +67,62 @@ export async function POST(request: NextRequest) {
       JSON.stringify({
         ok: true,
         newDischargeMaterials
+      }),
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        status: 200
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("🚀 ~ POST ~ error:", error);
+      return NextResponse.json(
+        {
+          ok: false,
+          message: error.message
+        },
+        {
+          status: 500
+        }
+      );
+    }
+  }
+}
+
+export async function PUT(request: NextRequest) {
+  const { ...dischargeMaterials }: IDischargeMaterials = await request.json();
+
+  const accessToken = request.headers.get("accessToken");
+  try {
+    if (!accessToken || !verifyJWT(accessToken)) {
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "Su sesión ha expirado, por favor autentiquese nuevamente"
+        },
+        {
+          status: 401
+        }
+      );
+    }
+    await connectDB();
+
+    const updatedDischargeMaterials = await DischargeMaterials.findOneAndUpdate(
+      { offerId: dischargeMaterials.offerId },
+      {
+        ...dischargeMaterials,
+        updatedAt: new Date()
+      },
+      { new: true }
+    );
+
+    return new NextResponse(
+      JSON.stringify({
+        ok: true,
+        updatedDischargeMaterials
       }),
       {
         headers: {
