c54cbd5bcc510eda759354643af2962a4a396c75
Daniel Muniz <danielnubiz902@gmail.com>
2025-03-01 09:00:20 -0500

    feat: :sparkles: Adding update material

---
+++ src/app/api/material/[warehouseId]/route.ts
@@ -0,0 +1,187 @@
+import { eq, and } from 'drizzle-orm';
+import { JwtPayload } from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+import { db } from "@/db/drizzle";
+import { materials } from "@/db/migrations/schema";
+import { verifyJWT } from "@/libs/jwt";
+import logger from "@/utils/logger";
+import { UpdateMaterial } from "@/types/DTOs/materials/materials";
+import moment from 'moment';
+
+export async function GET(request: NextRequest, { params }: { params: { warehouseId: number } }) {
+  const warehouseId = params.warehouseId; // Id del almacen
+  const accessToken = request.headers.get("accessToken");
+  try {
+    if (!accessToken || !verifyJWT(accessToken)) {
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "Su sesión ha expirado, por favor autentiquese nuevamente"
+        },
+        {
+          status: 401
+        }
+      );
+    }
+    const decoded = jwt.decode(accessToken) as JwtPayload;
+    logger.info("Listar Materiales", { method: request.method, url: request.url, user: decoded.userName });
+
+    const { searchParams } = new URL(request.url);
+    const page = parseInt(searchParams.get("page") || "1", 10); // Default to page 1
+    const limit = parseInt(searchParams.get("limit") || "10", 10); // Default to 10 items per page
+
+    if (isNaN(page) || isNaN(limit) || page < 1 || limit < 1) {
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "Parámetros de paginacion inválidos. 'page' y 'limit' deben ser mayor a 0."
+        },
+        {
+          status: 400
+        }
+      );
+    }
+
+    const offset = (page - 1) * limit;
+    const paginatedData = await db
+      .select()
+      .from(materials)
+      .where(eq(materials.warehouseId, warehouseId))
+      .orderBy(materials.id)
+      .limit(limit)
+      .offset(offset);
+    const totalCount = await db.$count(materials);
+
+    return new NextResponse(
+      JSON.stringify({
+        ok: true,
+        counter: paginatedData.length,
+        total: totalCount,
+        page,
+        limit,
+        data: paginatedData
+      }),
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        status: 200
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error("Error al listar los materiales", {
+        error: error.message,
+        stack: error.stack,
+        route: "/api/material/[warehouseId]",
+        method: "GET"
+      });
+      return NextResponse.json(
+        {
+          ok: false,
+          message: error.message
+        },
+        {
+          status: 500
+        }
+      );
+    }
+  }
+}
+
+export async function PUT(request: NextRequest, { params }: { params: { warehouseId: number } }) {
+  const warehouseId = params.warehouseId; // Id del almacen
+  const { ...materialToUpdate }: UpdateMaterial = await request.json();
+  const accessToken = request.headers.get("accessToken");
+  try {
+    if (!accessToken || !verifyJWT(accessToken)) {
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "Su sesión ha expirado, por favor autentiquese nuevamente"
+        },
+        {
+          status: 401
+        }
+      );
+    }
+    const decoded = jwt.decode(accessToken) as JwtPayload;
+    logger.info("Actualizar Material", { method: request.method, url: request.url, user: decoded.userName });
+
+    const { searchParams } = new URL(request.url);
+    const materialId = parseInt(searchParams.get("materialId")!); // Default to page 1
+
+    if (materialId < 1 || isNaN(materialId) || !searchParams.has("materialId")) {
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "MaterialId es requerido y no puede ser menor a 1."
+        },
+        {
+          status: 400
+        }
+      );
+    }
+
+    const isMaterialExist = await db.select().from(materials).where(and(eq(materials.id, materialId), eq(materials.warehouseId, warehouseId)));
+    if (isMaterialExist.length === 0) {
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "El material a actualizar no existe"
+        },
+        {
+          status: 404
+        }
+      );
+    }
+
+
+    // ? PARA ACTUALIZAR EL STOCK UTILIZE LAS FUNCIONES DE MOVER INVENTARIO
+    isMaterialExist[0].name = materialToUpdate.name;
+    isMaterialExist[0].category = materialToUpdate.category;
+    isMaterialExist[0].description = materialToUpdate.description;
+    isMaterialExist[0].modifyDate = new Date(); // Registra la fecha de modificación
+    isMaterialExist[0].unitMeasure = materialToUpdate.unitMeasure;
+    isMaterialExist[0].costPerUnit = materialToUpdate.costPerUnit;
+    isMaterialExist[0].minimumExistence = materialToUpdate.minimumExistence;
+    isMaterialExist[0].provider = materialToUpdate.provider;
+
+    const updatedMaterial = await db.update(materials).set(isMaterialExist[0]).where(eq(materials.id, materialId)).returning();
+
+    return new NextResponse(
+      JSON.stringify({
+        ok: true,
+        data: updatedMaterial
+      }),
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        status: 200
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(`Error al actualizar un material`, {
+        error: error.message,
+        stack: error.stack,
+        route: "/api/material/[warehouseId]",
+        method: "PUT"
+      });
+      return NextResponse.json(
+        {
+          ok: false,
+          message: error.message
+        },
+        {
+          status: 500
+        }
+      );
+    }
+  }
+}
--- src/types/DTOs/materials/materials.ts
+++ src/types/DTOs/materials/materials.ts
@@ -2,4 +2,7 @@ import { Material } from "@/db/migrations/schema";
 
 export type InsertMaterial = Omit<Material, "id" | "enterDate" | "modifyDate">;
 export type RemoveMaterial = { id: number; amount: number; warehouseId: number };
-// export type UpdateMaterial = Omit<Material, "totalValue">;
+export type UpdateMaterial = Omit<
+  Material,
+  "id" | "totalValue" | "warehouseId" | "serviceFeeMaterialNomenclatorId" | "stock" | "modifyDate" | "enterDate"
+>;
