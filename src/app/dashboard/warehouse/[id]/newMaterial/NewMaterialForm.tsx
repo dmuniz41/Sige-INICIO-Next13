493ad4a8a13ae7e264a1cb2160cb79f3ef42b56e
Daniel Muniz <danielnubiz902@gmail.com>
2025-03-08 10:31:18 -0500

    Adding style changes in add new material form

--- src/app/dashboard/warehouse/[id]/newMaterial/NewMaterialForm.tsx
+++ src/app/dashboard/warehouse/[id]/newMaterial/NewMaterialForm.tsx
@@ -35,79 +35,115 @@ export const NewMaterialForm = ({ warehouseId }: { warehouseId: string }) => {
   });
 
   return (
-    <Row className="flex flex-col ">
-      <section>
-        <Title level={2}>Nuevo Material</Title>
-      </section>
-      <Col span={10}>
-        <Form form={form} layout="vertical" name="newMaterialForm" size="large">
-          <Form.Item name="category" label={<Title level={4}>Categoría</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
-            <Select
-              allowClear
-              style={{ width: "100%" }}
-              options={category}
-              showSearch
-              optionFilterProp="children"
-              filterOption={(input: any, option: any) => (option?.label ?? "").toLowerCase().includes(input)}
-              filterSort={(optionA: any, optionB: any) =>
-                (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
-              }
-            />
-          </Form.Item>
-          <Form.Item name="materialName" label={<Title level={4}>Nombre del material</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="description" label={<Title level={4}>Descripción</Title>}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="costPerUnit" label={<Title level={4}>Costo por unidad de medida</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
-            <InputNumber min={0} className="w-full" />
-          </Form.Item>
-          <Form.Item name="unitMeasure" label={<Title level={4}>Unidad de medida</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
-            <Select
-              allowClear
-              style={{ width: "100%" }}
-              options={unitMeasure}
-              showSearch
-              optionFilterProp="children"
-              filterOption={(input: any, option: any) => (option?.label ?? "").toLowerCase().includes(input)}
-              filterSort={(optionA: any, optionB: any) =>
-                (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
-              }
-            />
-          </Form.Item>
-          <Form.Item name="unitsTotal" label={<Title level={4}>Cantidad a añadir</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
-            <InputNumber min={0} className="w-full" />
-          </Form.Item>
-          <Form.Item name="minimumExistence" label={<Title level={4}>Existencias mínimas</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
-            <InputNumber min={0} className="w-full" />
-          </Form.Item>
-          <Form.Item name="provider" label={<Title level={4}>Proveedor</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
-            <Select
-              allowClear
-              style={{ width: "100%" }}
-              options={provider}
-              showSearch
-              optionFilterProp="children"
-              filterOption={(input: any, option: any) => (option?.label ?? "").toLowerCase().includes(input)}
-              filterSort={(optionA: any, optionB: any) =>
-                (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
-              }
-            />
-          </Form.Item>
-          <Form.Item name="enterDate" label={<Title level={4}>Fecha de entrada</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
-            <DatePicker format={"MM/DD/YYYY"} />
-          </Form.Item>
-          <Row>
-          <Form.Item>
-            <Button>Añadir</Button>
-          </Form.Item>
-          <Form.Item>
-            <Button>Cancelar</Button>
-          </Form.Item>
+    <>
+      <Row className="py-4">
+        <Col span={12}>
+          <Title level={3}>Nuevo Material</Title>
+        </Col>
+        <Col span={12}>
+          <Row justify={"end"} gutter={16} className="flex gap-2 ">
+            <button className="toolbar-danger-icon-btn">Cancelar</button>
+            <button className="toolbar-primary-icon-btn" type="submit">
+              Guardar Material
+            </button>
           </Row>
-        </Form>
-      </Col>
-    </Row>
+        </Col>
+      </Row>
+      <Form form={form} layout="vertical" name="newMaterialForm" size="large">
+        <Row justify={"space-between"}>
+          <Col span={12} className="border-light border rounded-md py-4 px-2">
+            <div className="flex font-bold text-2xl mb-4 ml-2">
+              <span>General</span>
+            </div>
+            <Form.Item name="category" label={<Title level={5}>Categoría</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
+              <Select
+                allowClear
+                style={{ width: "100%" }}
+                options={category}
+                showSearch
+                optionFilterProp="children"
+                filterOption={(input: any, option: any) => (option?.label ?? "").toLowerCase().includes(input)}
+                filterSort={(optionA: any, optionB: any) =>
+                  (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
+                }
+              />
+            </Form.Item>
+            <Form.Item
+              name="materialName"
+              label={<Title level={5}>Nombre del material</Title>}
+              rules={[{ required: true, message: "Campo requerido" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item name="description" label={<Title level={5}>Descripción</Title>}>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="costPerUnit"
+              label={<Title level={5}>Costo por unidad de medida</Title>}
+              rules={[{ required: true, message: "Campo requerido" }]}
+            >
+              <InputNumber min={0} className="w-full" />
+            </Form.Item>
+            <Form.Item
+              name="unitMeasure"
+              label={<Title level={5}>Unidad de medida</Title>}
+              rules={[{ required: true, message: "Campo requerido" }]}
+            >
+              <Select
+                allowClear
+                style={{ width: "100%" }}
+                options={unitMeasure}
+                showSearch
+                optionFilterProp="children"
+                filterOption={(input: any, option: any) => (option?.label ?? "").toLowerCase().includes(input)}
+                filterSort={(optionA: any, optionB: any) =>
+                  (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
+                }
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12} className="border-light border rounded-md py-4 px-2">
+            <div className="flex font-bold text-2xl mb-4 ml-2">
+              <span>Detalles</span>
+            </div>
+            <Form.Item
+              name="unitsTotal"
+              label={<Title level={5}>Cantidad a añadir</Title>}
+              rules={[{ required: true, message: "Campo requerido" }]}
+            >
+              <InputNumber min={0} className="w-full" />
+            </Form.Item>
+            <Form.Item
+              name="minimumExistence"
+              label={<Title level={5}>Existencias mínimas</Title>}
+              rules={[{ required: true, message: "Campo requerido" }]}
+            >
+              <InputNumber min={0} className="w-full" />
+            </Form.Item>
+            <Form.Item name="provider" label={<Title level={5}>Proveedor</Title>} rules={[{ required: true, message: "Campo requerido" }]}>
+              <Select
+                allowClear
+                style={{ width: "100%" }}
+                options={provider}
+                showSearch
+                optionFilterProp="children"
+                filterOption={(input: any, option: any) => (option?.label ?? "").toLowerCase().includes(input)}
+                filterSort={(optionA: any, optionB: any) =>
+                  (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
+                }
+              />
+            </Form.Item>
+            <Form.Item
+              name="enterDate"
+              label={<Title level={5}>Fecha de entrada</Title>}
+              rules={[{ required: true, message: "Campo requerido" }]}
+            >
+              <DatePicker format={"MM/DD/YYYY"} />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </>
   );
 };
